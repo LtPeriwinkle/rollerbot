abd6785ed7d9ad557f7203427393e664d083f31e
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-04-23 17:10:50 -0700

    add error handling

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Monad (when)
 import qualified Data.List as L
+import Data.Maybe
 import qualified Data.Text as T
 import qualified Data.Text.Conversions as C
 import qualified Data.Text.IO as TIO
@@ -9,6 +10,7 @@ import Discord
 import qualified Discord.Requests as R
 import Discord.Types
 import System.Random
+import Text.Read
 import UnliftIO (liftIO)
 import UnliftIO.Concurrent
 
@@ -34,18 +36,25 @@ eventHandler event = case event of
   MessageCreate m -> when (not (isFromBot m) && isRoll m) $ do
     let dieInfo = parseDieInfo $ messageText m
     -- really bad way to get the number out of the Snowflake and turn it into a randomgen seed
-    let randList = genRandList dieInfo (mkStdGen (read $ show (messageId m) :: Int))
+    let randList = if isJust dieInfo then genRandList (fromJust dieInfo) (mkStdGen (read $ show (messageId m) :: Int)) else [0]
     let resultsField :: EmbedField
-        resultsField =
-          EmbedField
-            { embedFieldName = "Results",
-              embedFieldValue = C.convertText (concatMap (\x -> if x == "," then ", " else x) (L.group $ init (tail $ show randList))),
-              embedFieldInline = Just False
-            }
+        resultsField = case randList of
+          [0] ->
+            EmbedField
+              { embedFieldName = "Failed to parse dice roll",
+                embedFieldValue = "Please format your command as `!roll XdY` where X and Y are positive numbers.",
+                embedFieldInline = Just False
+              }
+          xs ->
+            EmbedField
+              { embedFieldName = "Results",
+                embedFieldValue = C.convertText (concatMap (\x -> if x == "," then ", " else x) (L.group $ init (tail $ show randList))),
+                embedFieldInline = Just False
+              }
     let embed :: CreateEmbed
         embed =
           def
-            { createEmbedTitle = C.convertText ("rolled " ++ show (fst dieInfo) ++ "d" ++ show (snd dieInfo)),
+            { createEmbedTitle = if isJust dieInfo then C.convertText ("rolled " ++ show (fst $ fromJust dieInfo) ++ "d" ++ show (snd $ fromJust dieInfo)) else "Error",
               createEmbedFields = [resultsField]
             }
     let opts :: R.MessageDetailedOpts
@@ -64,18 +73,23 @@ isFromBot :: Message -> Bool
 isFromBot m = userIsBot $ messageAuthor m
 
 isRoll :: Message -> Bool
-isRoll m = "!roll" `T.isPrefixOf` messageText m
+isRoll m = "!roll " `T.isPrefixOf` messageText m
 
 genRandList :: (Int, Int) -> StdGen -> [Int]
 genRandList (a, b)
   | a == 0 || b == 0 = empty
-  | otherwise = take a . L.unfoldr (Just . uniformR (0, b))
+  | otherwise = take a . L.unfoldr (Just . uniformR (1, b))
 
 -- lol
 empty :: StdGen -> [Int]
 empty g = [0]
 
-parseDieInfo :: T.Text -> (Int, Int)
+parseDieInfo :: T.Text -> Maybe (Int, Int)
 parseDieInfo m = do
   let split = span (/= 'd') $ dropWhile (`elem` ("!roll " :: [Char])) (C.convertText m :: String)
-  (read (fst split) :: Int, read (tail $ snd split) :: Int)
+  if snd split /= ""
+    then
+      let count = readMaybe $ fst split :: Maybe Int
+          sides = readMaybe (tail $ snd split) :: Maybe Int
+       in if isJust count && isJust sides then Just (fromJust count, fromJust sides) else Nothing
+    else Nothing
